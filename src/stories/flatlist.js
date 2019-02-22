c247dcfa4b78599d91b6511905c0fbf84f5de8d4
Michiel <Michiel_87@hotmail.com>
2019-02-22 11:28:55 +0100

    second commit

--- src/stories/flatlist.js
+++ src/stories/flatlist.js
@@ -3,12 +3,5 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 
-import TextInput from '../components/Input/TextInput'
-
-let textInputValue = "Hello there!"
-
 export default storiesOf('Flatlist', module)
-  .add('default', () => <TextInput value={textInputValue} onChange={(value) => {
-    textInputValue = value
-    action('New Value: ' + value)
-  }}>Hello Button</TextInput>)
+  .add('default', () =>  null)
