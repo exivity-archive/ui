bc19ed51728c24b3320e2b6e95d95758503a814c
Michiel <Michiel_87@hotmail.com>
2019-02-22 10:43:41 +0100

    Added withState decorator for storybook

--- src/stories/input.js
+++ src/stories/input.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { withState } from '../../.storybook/StateDecorator'
 
 import TextInput from '../components/Input/TextInput'
 import Checkbox from '../components/Input/Checkbox'
 import Button from '../components/Input/Button'
 
-let textInputValue = "Hello there!"
-
 export default storiesOf('Input', module)
-  .add('TextInput', () => <TextInput value={textInputValue} onChange={(value) => {
-    textInputValue = value
-    action('New Value: ' + value)
+  .addDecorator(withState('Testing'))
+  .add('TextInput', ({ state, storeState }) => <TextInput value={state} onChange={(value) => {
+    storeState(value)
+    action('typing')(value)
   }}>Hello Button</TextInput>)
   .add('Checkbox', () => <Checkbox onClick={action('clicked')}/>)
   .add('Button', () => <Button onClick={action('clicked')}>Hello Button</Button>)
