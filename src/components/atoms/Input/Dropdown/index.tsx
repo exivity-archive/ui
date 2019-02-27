a40b8f36767e0646109f32523aabc8f6758afe76
StefanWullems <stefan@exivity.com>
2019-02-27 17:42:49 +0100

    different box shadow

--- src/components/atoms/Input/Dropdown/index.tsx
+++ src/components/atoms/Input/Dropdown/index.tsx
@@ -16,7 +16,7 @@ const DropdownContent = styled.div<IDropdownContentProps>`
   position: absolute;
   background-color: #f9f9f9;
   min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.1);
+  box-shadow: 0 5px 15px rgba(0,0,0,0.08);
   padding: 20px;
   padding-right: 10%;
   ${ ({position}) => css`
