f1a308ea245d8ecc13e7e06f15369ef44f031ef0
StefanWullems <stefan@exivity.com>
2019-03-01 14:36:57 +0100

    defined types and made dummy implementation

---
+++ src/useDynamicAttribute/index.test.tsx
@@ -0,0 +1,8 @@
+import useDynamicAttribute from '.'
+
+test('length of enriched array should be the same as the passed in array', () => {
+  // interface ITest { id: number }
+  // const test = [{ id: 1 },{ id: 2 }, { id: 3 }]
+
+  // const enriched = useDynamicAttribute<ITest,
+})
--- src/useDynamicAttribute/index.tsx
+++ src/useDynamicAttribute/index.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
 
-function useDynamicAttribute<T extends object, V> (data: T, key: string, itemAccessor: V | ((item: T) => V)): any {
-  return 'hi'
+type Enriched<TKey extends string, TSet extends string, TValue> = Record<TKey, TValue> & Record<TSet, (value: TValue) => void>
+
+function useDynamicAttribute<
+  T extends {},
+  TKey extends string,
+  TSet extends string,
+  TValue
+> (data: T[], key: TKey, set: TSet, initVal: ((item: T) => TValue) | TValue): (T & Enriched<TKey, TSet, TValue>)[] {
+  const data1: T = data[0]
+  const data2 = { [key]: initVal, [set]: (value: TValue) => { return } } as Enriched<TKey, TSet, TValue>
+  return [{
+    ...data1,
+    ...data2
+  }]
 }
+
+export default useDynamicAttribute
