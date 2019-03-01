5ea8633d398a756bc18e48fa0749650f35e1b862
StefanWullems <stefan@exivity.com>
2019-03-01 15:33:36 +0100

    logix

--- src/useDynamicAttribute/index.tsx
+++ src/useDynamicAttribute/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-type Enriched<TKey extends string, TSet extends string, TValue> = Record<TKey, TValue> & Record<TSet, (value: TValue) => void>
+type InitCallback<T, TValue> = ((item: T) => TValue)
+type Set<TValue> = (value: TValue) => void
+type Enriched<TKey extends string, TSet extends string, TValue> = Record<TKey, TValue> & Record<TSet, Set<TValue>>
 
 function useDynamicAttribute<
   T extends {},
   TKey extends string,
   TSet extends string,
   TValue
-> (data: T[], key: TKey, set: TSet, initVal: ((item: T) => TValue) | TValue): (T & Enriched<TKey, TSet, TValue>)[] {
-  const data1: T = data[0]
-  const data2 = { [key]: initVal, [set]: (value: TValue) => { return } } as Enriched<TKey, TSet, TValue>
-  return [{
-    ...data1,
-    ...data2
-  }]
+> (data: T[], key: TKey, set: TSet, initVal: InitCallback<T, TValue> | TValue): (T & Enriched<TKey, TSet, TValue>)[] {
+
+  function getInitialState (item: T) {
+    if (typeof initVal !== 'function') {
+      return initVal
+    }
+    return (initVal as InitCallback<T, TValue>)(item)
+  }
+
+  return data.map(item => {
+    const [value, setValue] = useState(getInitialState(item))
+    const enrichment = { [key]: value, [set]: setValue } as Enriched<TKey, TSet, TValue>
+    return {
+      ...item,
+      ...enrichment
+    }
+  })
+
 }
 
 export default useDynamicAttribute
