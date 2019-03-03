19bb6cd010d615511987fec9ce8ae9c80097c0d6
Joram van den Boezem <joram@vandenboezem.nl>
2019-03-03 21:19:43 +0100

    refactor: make label more isolated
    Functionality will be replaced by Field component later on.

--- src/Label/Label.stories.js
+++ src/Label/Label.stories.js
@@ -4,6 +4,6 @@ import Label from './Label'
 import { storiesOf } from '@storybook/react'
 
 export default storiesOf('atoms|Label', module)
-  .add('Label', () => {
-    return <Label name='Label name' description='Label description' />
-  })
+  .add('default', () => <Label>Default label</Label>)
+  .add('secondary', () => <Label secondary>Secondary label</Label>)
+  .add('Nested', () => <Label>Primary label<Label>Secondary label</Label></Label>)
