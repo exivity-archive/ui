30c635789ac049f7b5081596c9a6d564af6cea9f
Joram van den Boezem <joram@vandenboezem.nl>
2019-03-06 15:04:18 +0100

    refactor: bring back fromTheme (#26)

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
 import Icon from '../Icon'
-import { defaultStyledProps, hexToString, matchThemeProp, StyledProps } from '../utils/styled'
+import { defaultStyledProps, fromTheme, hexToString, matchThemeProp, StyledProps } from '../utils/styled'
 import { preciseEm } from '../utils/styled/isolated'
 
 export interface ButtonProps extends StyledProps {
@@ -20,14 +20,14 @@ export interface ButtonProps extends StyledProps {
 }
 
 export const Button = styled.button <ButtonProps>`
-  font-family: ${props => props.theme.global.fontFamily};
+  font-family: ${fromTheme(theme => theme.global.fontFamily)};
   font-weight: 500;
   font-size: ${matchThemeProp(theme => theme.global.sizes, {
     modifier: (em: number) => em * 14,
     defaultValue: 14
   })}px;
-  color: ${props => props.theme.colours.white};
-  line-height: ${props => props.theme.global.lineHeight};
+  color: ${fromTheme(theme => theme.colours.white)};
+  line-height: ${fromTheme(theme => theme.global.lineHeight)};
 
   text-transform: uppercase;
   display: inline-flex;
@@ -40,7 +40,7 @@ export const Button = styled.button <ButtonProps>`
   height: ${preciseEm(2.85, 14)}em; // 40px = 2.5 * 16
   padding: 0 ${preciseEm(1.5)}em;
   border: none;
-  border-radius: ${props => props.theme.global.borderRadius}px;
+  border-radius: ${fromTheme(theme => theme.global.borderRadius)}px;
   flex: none;
   user-select: none;
   white-space: nowrap;
@@ -51,28 +51,28 @@ export const Button = styled.button <ButtonProps>`
   --focus-color: ${matchThemeProp(theme => theme.global.purposes, { modifier: hexToString })};
 
   &:hover {
-    box-shadow: inset 10px 10px 999em rgba(0,0,0,${props => props.theme.global.shadowAlpha});
+    box-shadow: inset 10px 10px 999em rgba(0,0,0,${fromTheme(theme => theme.global.shadowAlpha)});
   }
 
   &:focus {
-    box-shadow: inset 0 0 999em rgba(0,0,0,${props => props.theme.global.shadowAlpha}),
-      0 0 0 ${props => props.theme.global.outlineWidth}px rgba(var(--focus-color), ${props => props.theme.global.outlineAlpha});
+    box-shadow: inset 0 0 999em rgba(0,0,0,${fromTheme(theme => theme.global.shadowAlpha)}),
+      0 0 0 ${fromTheme(theme => theme.global.outlineWidth)}px rgba(var(--focus-color), ${fromTheme(theme => theme.global.outlineAlpha)});
   }
 
   &:active,
   &.active {
-    box-shadow: inset 0 0 999em rgba(0,0,0,${props => props.theme.global.shadowAlpha * 2}),
-      0 0 0 ${props => props.theme.global.outlineWidth}px rgba(var(--focus-color), ${props => props.theme.global.outlineAlpha});
+    box-shadow: inset 0 0 999em rgba(0,0,0,${fromTheme(theme => theme.global.shadowAlpha * 2)}),
+      0 0 0 ${fromTheme(theme => theme.global.outlineWidth)}px rgba(var(--focus-color), ${fromTheme(theme => theme.global.outlineAlpha)});
   }
 
   &:after {
     display: none;
     content: "";
     position: absolute;
-    top: -${props => props.theme.global.borderWidth}px;
-    right: -${props => props.theme.global.borderWidth}px;
-    bottom: -${props => props.theme.global.borderWidth}px;
-    left: -${props => props.theme.global.borderWidth}px;
+    top: -${fromTheme(theme => theme.global.borderWidth)}px;
+    right: -${fromTheme(theme => theme.global.borderWidth)}px;
+    bottom: -${fromTheme(theme => theme.global.borderWidth)}px;
+    left: -${fromTheme(theme => theme.global.borderWidth)}px;
     border-radius: inherit;
     background-color: rgba(255, 255, 255, 0.5);
   }
@@ -85,23 +85,23 @@ export const Button = styled.button <ButtonProps>`
   }
 
   ${props => props.outlined && css`
-    background-color: ${props => props.theme.colours.white};
+    background-color: ${fromTheme(theme => theme.colours.white)};
     color: ${matchThemeProp(theme => theme.global.purposes)};
-    box-shadow: 0 0 0 ${props => props.theme.global.borderWidth}px ${matchThemeProp(theme => theme.global.purposes)};
+    box-shadow: 0 0 0 ${fromTheme(theme => theme.global.borderWidth)}px ${matchThemeProp(theme => theme.global.purposes)};
 
     &:hover {
-      color: ${props => props.theme.global.textColor};
-      box-shadow: 0 0 0 ${props => props.theme.global.borderWidth}px ${props => props.theme.global.textColor};
+      color: ${fromTheme(theme => theme.global.textColor)};
+      box-shadow: 0 0 0 ${fromTheme(theme => theme.global.borderWidth)}px ${fromTheme(theme => theme.global.textColor)};
     }
 
     &:focus {
-      box-shadow: 0 0 0 ${props => props.theme.global.borderWidth}px ${matchThemeProp(theme => theme.global.purposes)},
-       0 0 0 ${props => props.theme.global.outlineWidth + 1}px rgba(var(--focus-color), ${props => props.theme.global.outlineAlpha});
+      box-shadow: 0 0 0 ${fromTheme(theme => theme.global.borderWidth)}px ${matchThemeProp(theme => theme.global.purposes)},
+       0 0 0 ${fromTheme(theme => theme.global.outlineWidth + 1)}px rgba(var(--focus-color), ${fromTheme(theme => theme.global.outlineAlpha)});
     }
   `}
 
   ${Icon} {
-    margin-right: ${props => props.theme.global.spacing / 2}em;
+    margin-right: ${fromTheme(theme => theme.global.spacing / 2)}em;
   }
 `
 
