1dc1374ca686fdd491e7e91227eb1d72255c28aa
Michiel de Vos <Michiel_87@hotmail.com>
2019-03-08 17:01:26 +0100

    Modal (#36)
    * Modal component
    
    * removed styled props
    
    * overlay and scrollable
    
    * updated
    
    * possible fix
    
    * revert
    
    * clean
    
    * updated tests

--- src/Modal/Modal.stories.tsx
+++ src/Modal/Modal.stories.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+// @ts-ignore
 import { storiesOf } from '@storybook/react'
 import { Modal } from './'
 import { loremIpsum } from '../utils/loremIpsum'
-import { Button } from '../Button/Button'
+import { Button } from '../Button'
 
 storiesOf('molecules|Modal', module)
   .add('default', () => {
