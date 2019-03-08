0dc42df7ce44508242ad099d126aafbf58be6580
stefanwullems <stefan@exivity.com>
2019-03-08 09:52:08 +0100

    overlay and scrollable

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -1,15 +1,24 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
-import { fromTheme, defaultStyledProps, StyledProps } from '../utils/styled'
-import { Theme } from '../themes'
+import { fromTheme } from '../utils/styled'
+
+const Overlay = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0,0,0,0.1);
+  position: absolute;
+`
 
 const ModalWrapper = styled.div`
   position: absolute;
   width: 600px;
+  background-color: ${fromTheme(theme => theme.colours.white)};
+  left: calc((100vw - 600px) / 2);
+  top: 50px;
 `
 
 const Header = styled.div`
-  padding: 15px 30px;
+  padding: 25px 30px 20px 30px;
   color: ${fromTheme(theme => theme.colours.gray)};
   font-size: 28px;
   font-weight: 700;
@@ -20,10 +29,11 @@ const Body = styled.div`
   height: 400px;
   border-top: solid 1px ${fromTheme(theme => theme.colours.gray)};
   border-bottom: solid 1px ${fromTheme(theme => theme.colours.gray)};
+  overflow-y: auto;
 `
 
 const Footer = styled.div`
-  padding: 20px 30px 20px 30px;
+  padding: 15px 30px;
   color: ${fromTheme(theme => theme.colours.gray)};
   display: flex;
   flex-direction: row-reverse;
@@ -40,9 +50,11 @@ interface ModalProps {
 }
 
 export const Modal: FC<ModalProps> = ({ title, text, buttons = [] }) => (
-  <ModalWrapper>
-    <Header>{title}</Header>
-    <Body>{text}</Body>
-    <Footer>{buttons.map((button, i) => (<ButtonWrapper key={i}>{button}</ButtonWrapper>))}</Footer>
-  </ModalWrapper>
+  <Overlay>
+    <ModalWrapper>
+      <Header>{title}</Header>
+      <Body>{text}</Body>
+      <Footer>{buttons.map((button, i) => (<ButtonWrapper key={i}>{button}</ButtonWrapper>))}</Footer>
+    </ModalWrapper>
+  </Overlay>
 )
