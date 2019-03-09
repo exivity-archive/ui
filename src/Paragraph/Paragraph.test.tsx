93bff5eab4ea028d99e2b53a09f0c64df15455e1
Joram van den Boezem <joram@vandenboezem.nl>
2019-03-09 20:43:48 +0100

    test: add seed to faker

--- src/Paragraph/Paragraph.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import { Paragraph } from '.'
-
-test('renders Paragraph without props', () => {
-  const component = renderer.create(<Paragraph />)
-  expect(component.toJSON()).toMatchSnapshot()
-})
