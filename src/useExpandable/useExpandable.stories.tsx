52abf9feaf266e00366c200f2faa3fdc443b97bc
Michiel <Michiel_87@hotmail.com>
2019-03-14 15:38:46 +0100

    story

--- src/useExpandable/useExpandable.stories.tsx
+++ src/useExpandable/useExpandable.stories.tsx
@@ -3,17 +3,14 @@ import { FixedSizeList } from 'react-window'
 
 // @ts-ignore
 import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
 
 import { useExpandable, TreeListItem } from '.'
 import { FakeRecord, FLAT_LIST_TEST_DATA } from './stories/seed'
 
 const getParent = (item: FakeRecord) => item.parentId
-const keys = ['1']
+const keys = ['1', '101', '201']
 const ExpandableList = () => {
-  console.time('start')
   const [data] = useExpandable<FakeRecord>(FLAT_LIST_TEST_DATA, getParent, keys)
-  console.timeEnd('start')
 
   return (
     <FixedSizeList height={600} width={400} itemSize={50} itemData={data} itemCount={data.length} >
@@ -25,8 +22,7 @@ const ExpandableList = () => {
 const Item = ({ data, index, style }: { data: TreeListItem<FakeRecord>[], index: number, style: object}) => {
   const item = data[index]
   const space = new Array(item.attributes.level)
-  // // @ts-ignore
-  // console.log(item.children)
+
   return useMemo(() => {
     return (
       <div onClick={item.expand} style={style}>
