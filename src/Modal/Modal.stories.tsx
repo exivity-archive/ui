b263836f15f72f629a9feb0b317c5b70f3cf25fe
Stefan Wullems <stefan@exivity.com>
2019-03-18 17:05:49 +0100

    feat: modal (#37)
    * Modal component
    
    * removed styled props
    
    * overlay and scrollable
    
    * updated
    
    * possible fix
    
    * revert
    
    * added snapshot
    
    * added snapshot
    
    * implemented requested changes
    
    * rm unintended file
    
    * removed lorem ipsum
    
    * implemented requested changes
    
    * rm unintende file
    
    * rm loremIpsum

--- src/Modal/Modal.stories.tsx
+++ src/Modal/Modal.stories.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Faker from 'faker'
+
 // @ts-ignore
 import { storiesOf } from '@storybook/react'
 import { Modal } from './'
-import { loremIpsum } from '../utils/loremIpsum'
 import { Button } from '../Button'
 
 storiesOf('molecules|Modal', module)
@@ -13,6 +14,7 @@ storiesOf('molecules|Modal', module)
         buttons={[
           <Button onClick={() => { return }}>Whatsup</Button>,
           <Button danger onClick={() => { return }}>Delete</Button>
-        ]}>{loremIpsum}</Modal>
+        ]
+        }> {Faker.lorem.paragraph(30)}</Modal >
     )
   })
