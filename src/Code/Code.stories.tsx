6da06b0616942fdcc92ec56da03897106618c3db
Joram van den Boezem <joram@vandenboezem.nl>
2019-03-19 22:52:03 +0100

    test(coverage): increase test surface

--- src/Code/Code.stories.tsx
+++ src/Code/Code.stories.tsx
@@ -11,3 +11,4 @@ const code = `import { CodeBlock } from '@exivity/ui'
 storiesOf('atoms|Code', module)
   .add('default', () => <Paragraph>This is some <Code>inline code</Code> right here.</Paragraph>)
   .add('block', () => <Code block language='jsx'>{code}</Code>)
+  .add('non-existing', () => <Code block language='exivity-js'>Unsupported language displayed as text</Code>)
--- src/utils/randomId.test.ts
+++ src/utils/randomId.test.ts
@@ -1,8 +1,19 @@
 import { randomId } from './randomId'
 
-test('randomId generates predictable numbers during tests', () => {
+test('randomId generates predictable numbers in test environments', () => {
   const first = Number(randomId())
   const second = Number(randomId())
 
   expect(second).toEqual(first + 1)
+})
+
+test('randomId generates random numbers in non-test environments', () => {
+  const oldEnv = process.env.NODE_ENV
+  process.env.NODE_ENV = 'production'
+  const random = randomId()
+
+  expect(random.length).toEqual(8)
+
+  // Reset
+  process.env.NODE_ENV = oldEnv
 })
