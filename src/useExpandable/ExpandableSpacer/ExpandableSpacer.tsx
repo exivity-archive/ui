bb8190a64e5357715b096b6b41075370827177d3
stefanwullems <stefan@exivity.com>
2019-03-20 17:03:17 +0100

    spacer centered

--- src/useExpandable/ExpandableSpacer/ExpandableSpacer.tsx
+++ src/useExpandable/ExpandableSpacer/ExpandableSpacer.tsx
@@ -1,27 +1,28 @@
-import React from 'react'
+import React, { useRef, useEffect, useState, cloneElement } from 'react'
 import styled, { css } from 'styled-components'
 import { fromTheme } from '../../utils/styled'
+import { ButtonProps } from '../../Button'
 
 interface StyledExpandableSpacerProps {
   leftSpacerLine: number
   level: number
   distance: number
+  spacing: number
 }
 
 export const StyledExpandableSpacer = styled.div<StyledExpandableSpacerProps>`
-  margin-left: ${({ level }) => (level * 40)}px;
-  margin-right: 20px;
+  margin-left: ${({ spacing, level }) => (spacing * 1.5) * level}px;
   height: 100%;
 
-  ${({ leftSpacerLine, distance }) => css`&:after {
+  ${({ leftSpacerLine, distance, spacing }) => css`&:after {
       position: relative;
-      left: -20px;
       top: calc(-${(100 * distance)}% - 50%);
+      right: ${spacing}px;
       border: solid ${fromTheme(theme => theme.colours.lightGray)};
       border-width: 0px 0px 1px ${leftSpacerLine}px;
       content: ' ';
       display: block;
-      width: 20px;
+      width: ${spacing}px;
       height: ${distance * 100}%;
       z-index: -1;
     }`
@@ -36,19 +37,32 @@ const Content = styled.div`
 `
 
 interface ExpandableSpacerProps {
-  button?: React.ReactComponentElement<'button', {}> | null
+  button?: React.ReactComponentElement<'button', ButtonProps> | null
   index: number
   level: number
   distance: number
+  hasChildren: boolean
 }
 
-export const ExpandableSpacer: React.FC<ExpandableSpacerProps> = ({ button, children, index, level, distance }) => {
+export const ExpandableSpacer: React.FC<ExpandableSpacerProps> = ({ children, index, level, distance, button, hasChildren }) => {
+  const buttonRef = useRef<HTMLDivElement>(null)
+  const [left, setLeft] = useState(0)
+
+  useEffect(() => {
+    if (buttonRef.current) {
+      setLeft((buttonRef.current.getBoundingClientRect().width))
+    }
+  }, [level])
+
   return (
     <StyledExpandableSpacer
       leftSpacerLine={index > 0 ? 1 : 0}
       level={level}
+      spacing={left}
       distance={distance}>
-      <Content>{button}{children}</Content>
+      <Content >{!(!hasChildren && left !== 0) &&
+        <div ref={buttonRef}>{button}</div>
+      }{children}</Content>
     </StyledExpandableSpacer>
   )
 }
