a981e951a57f50f7fe0939b4e35eeb0235044ee8
Michiel <Michiel_87@hotmail.com>
2019-03-20 13:55:57 +0100

    updated tests

--- src/Select/Select.stories.tsx
+++ src/Select/Select.stories.tsx
@@ -5,6 +5,9 @@ import { withState } from '../utils/tests/decorators/StateDecorator'
 
 import { Select } from '.'
 import { SelectList } from '../SelectList'
+import { TextInput } from '../TextInput'
+import { SelectInput } from '../SelectInput'
+import { Row } from '../utils/stories/components'
 
 const items = [
   { key: '1', value: 'one' },
@@ -17,6 +20,39 @@ export default storiesOf('molecules/Select', module)
   .addDecorator(withState())
   .add('default', ({ state, storeState }: any) => (
     <Select value={state && state.value}>
+      <SelectList value={state && state.key} onChange={storeState} data={items}/>
+    </Select>
+  ))
+  .add('Custom valueComponent', ({ state, storeState }: any) => (
+    <Row columns={4}>
+      <Select value={state && state.value} valueComponent={<TextInput placeholder='Choose option' outlined/>}>
+        <SelectList value={state && state.key} onChange={storeState} data={items}/>
+      </Select>
+      <Select value={state && state.value} valueComponent={<TextInput secondary placeholder='Choose option' outlined/>}>
+        <SelectList value={state && state.key} onChange={storeState} data={items}/>
+      </Select>
+      <Select value={state && state.value} valueComponent={<TextInput danger placeholder='Choose option' outlined/>}>
+        <SelectList value={state && state.key} onChange={storeState} data={items}/>
+      </Select>
+      <Select value={state && state.value} valueComponent={<TextInput success placeholder='Choose option' outlined/>}>
+        <SelectList value={state && state.key} onChange={storeState} data={items}/>
+      </Select>
+      <Select value={state && state.value} valueComponent={<SelectInput placeholder='Choose option'/>}>
+        <SelectList value={state && state.key} onChange={storeState} data={items}/>
+      </Select>
+      <Select value={state && state.value} valueComponent={<SelectInput placeholder='Choose option'/>}>
+        <SelectList value={state && state.key} onChange={storeState} data={items}/>
+      </Select>
+      <Select value={state && state.value} valueComponent={<SelectInput danger placeholder='Choose option'/>}>
+        <SelectList value={state && state.key} onChange={storeState} data={items}/>
+      </Select>
+      <Select value={state && state.value} valueComponent={<SelectInput success placeholder='Choose option'/>}>
+        <SelectList value={state && state.key} onChange={storeState} data={items}/>
+      </Select>
+    </Row>
+  ))
+  .add('useTriggerComponentWidth = false', ({ state, storeState }: any) => (
+    <Select value={state && state.value} useTriggerComponentWidth={false}>
       <SelectList value={state && state.key} onChange={storeState} data={items}/>
     </Select>
   ))
