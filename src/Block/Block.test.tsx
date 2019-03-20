de5ff9da20af9774fe78af0865d7ec5a6d2680c5
Joram van den Boezem <joram@vandenboezem.nl>
2019-03-20 16:18:45 +0100

    chore: update snapshots

--- src/Block/Block.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import { Block } from '.'
-
-// Modify or remove, stories are snapshotted automatically
-test('renders Block without props', () => {
-  const component = renderer.create(<Block />)
-  expect(component.toJSON()).toMatchSnapshot()
-})
