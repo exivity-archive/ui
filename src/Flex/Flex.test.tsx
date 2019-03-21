d1b496b42625f98c75c4b896bb8db2ee541000cb
Joram van den Boezem <joram@vandenboezem.nl>
2019-03-21 14:53:20 +0100

    chore: update snapshots

--- src/Flex/Flex.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import { Flex } from '.'
-
-// Modify or remove, stories are snapshotted automatically
-test('renders Flex without props', () => {
-  const component = renderer.create(<Flex />)
-  expect(component.toJSON()).toMatchSnapshot()
-})
