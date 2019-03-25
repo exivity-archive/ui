86857e7c605193952a4d7cfb24b5d1eb485872fe
Michiel <Michiel_87@hotmail.com>
2019-03-25 14:34:00 +0100

    updated buttons

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -78,6 +78,7 @@ export const Button = styled.button<ButtonProps>`
   }
 
   ${(props: ButtonProps) => props.outlined && css`
+    background-color: transparent;
     color: ${matchThemeProp(theme => theme.global.purposes)};
     box-shadow: 0 0 0 ${fromTheme(theme => theme.global.borderWidth)}px ${matchThemeProp(theme => theme.global.purposes)};
 
