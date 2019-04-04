c5baec90f12e02631783f8d7eb7c29ac46d93930
stefanwullems <stefan@exivity.com>
2019-04-04 19:10:35 +0200

    rm nonexistent imports

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 
 import { SelectInput } from '../SelectInput'
 import { Dropdown } from '../Dropdown'
-import { Horizontal, Vertical } from '../Dropdown/helpers'
 import { BlockProps } from '../Block'
+import { Vertical, Horizontal } from '../useEdgeAvoidingLayout'
 
 interface InjectValueAndHandler {
   name?: string
@@ -36,24 +36,24 @@ export const injectComponent = (component: React.ReactElement<any>, props: Injec
 const getTriggerComponent = (props: InjectValueAndHandler, valueComponent?: React.ReactElement<any>) => {
   if (valueComponent) return injectComponent(valueComponent, props)
   // Does not need onChange because SelectInput only display data
-  return <SelectInput {...props}/>
+  return <SelectInput {...props} />
 }
 
 export const Select: React.FC<SelectProps> = ({
-    name,
-    value,
-    placeholder,
-    onChange,
-    valueComponent,
-    useTriggerComponentWidth = true,
-    onOutsideClick,
-    vertical,
-    horizontal,
-    children,
-    py = 2,
-    test,
-    ...blockProps
-  }) => {
+  name,
+  value,
+  placeholder,
+  onChange,
+  valueComponent,
+  useTriggerComponentWidth = true,
+  onOutsideClick,
+  vertical,
+  horizontal,
+  children,
+  py = 2,
+  test,
+  ...blockProps
+}) => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const close = () => setIsOpen(false)
 
@@ -68,10 +68,10 @@ export const Select: React.FC<SelectProps> = ({
 
   return (
     <Dropdown open={isOpen} vertical={vertical} horizontal={horizontal} {...blockProps} py={py}
-              onOutsideClick={() => onOutsideClick ? onOutsideClick(isOpen, close) : setIsOpen(false)}
-              triggerComponent={triggerComponent}
-              useTriggerComponentWidth={useTriggerComponentWidth}
-              test={test}>
+      onOutsideClick={() => onOutsideClick ? onOutsideClick(isOpen, close) : setIsOpen(false)}
+      triggerComponent={triggerComponent}
+      useTriggerComponentWidth={useTriggerComponentWidth}
+      test={test}>
       {React.cloneElement(children, {
         ...children.props,
         onChange: (value: any) => {
