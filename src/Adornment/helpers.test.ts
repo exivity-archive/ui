1c2f9a2d193424d97aed47e1ef20c21f11f58da9
stefanwullems <stefan@exivity.com>
2019-06-14 10:21:58 +0200

    tests for helpers

--- src/Adornment/helpers.test.ts
+++ src/Adornment/helpers.test.ts
@@ -1,4 +1,5 @@
-import { makeCssCalcStatement } from './helpers'
+import { makeCssCalcStatement, mergeExtraPadding } from './helpers'
+import { Position } from './Adornment'
 
 describe('makeCssCalcStatement(...)', () => {
   test('it returns a calc statement containing all arguments', () => {
@@ -8,5 +9,25 @@ describe('makeCssCalcStatement(...)', () => {
 
     expect(result).toBe(`calc(1px + 5em + 100%)`)
   })
+})
 
+describe('mergeExtraPadding(...)', () => {
+  test('merges the values of all extra paddings into calc statements', () => {
+    const extraPaddingA = {
+      [Position.LEFT]: '20px',
+      [Position.RIGHT]: '4em'
+    }
+
+    const extraPaddingB = {
+      [Position.LEFT]: '10%',
+      [Position.RIGHT]: '4em'
+    }
+
+    const args = [extraPaddingA, extraPaddingB]
+
+    const result = mergeExtraPadding(...args)
+
+    expect(result[Position.LEFT]).toBe('calc(20px + 10%)')
+    expect(result[Position.RIGHT]).toBe('calc(4em + 4em)')
+  })
 })
