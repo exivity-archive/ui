f0ff5ab544d8ca76a2bf4692ed06ae8eabff8244
ThiagoFazzi <thiago@xtwo.com.br>
2019-06-18 16:52:22 +0200

    fix Horizontal/Vertical to use enum on the helpers test file

--- src/Dropdown/helpers.test.ts
+++ src/Dropdown/helpers.test.ts
@@ -1,9 +1,10 @@
-import { makeCssPosition } from './helpers'
+import { makeDefaultCSS } from './helpers'
+import { Vertical, Horizontal } from '../useSnapEdgeToParent'
 
 test('makeCssPosition should return correct css for given inputs', () => {
   const expected1 = 'top: 10px;\nleft: 0px;\n'
   const expected2 = 'bottom: 10px;\nright: 0px;\n'
 
-  expect(makeCssPosition({ horizontal: 'left', vertical: 'top' }, 10)).toBe(expected1)
-  expect(makeCssPosition({ horizontal: 'right', vertical: 'bottom' }, 10)).toBe(expected2)
+  expect(makeDefaultCSS({ horizontal: Horizontal.LEFT, vertical: Vertical.TOP }, 10)).toBe(expected1)
+  expect(makeDefaultCSS({ horizontal: Horizontal.RIGHT, vertical: Vertical.BOTTOM }, 10)).toBe(expected2)
 })
