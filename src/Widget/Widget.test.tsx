2e0ce3a5f15b6f79881f59cfb4b8d9408dd77d91
stefanwullems <stefan@exivity.com>
2019-06-19 10:19:33 +0200

    tests for widget

---
+++ src/Widget/Widget.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Widget } from './Widget'
+import { render, cleanup } from '@testing-library/react'
+
+afterEach(cleanup)
+
+describe('<Widget/>', () => {
+  test('It renders without errors', () => {
+    render(<Widget><div /></Widget>)
+  })
+
+  test('sticky prop should set position to sticky', () => {
+    const { getByText } = render(<Widget sticky>Test</Widget>)
+    const widget = getByText('Test')
+    expect(widget).toHaveStyleRule('position', 'sticky')
+  })
+
+  test('stickyOffset defaults to 0', () => {
+    const { getByText } = render(<Widget sticky>Test</Widget>)
+    const widget = getByText('Test')
+    expect(widget).toHaveStyleRule('top', '0px')
+  })
+
+  test('stickyOffset prop sets the top property if sticky prop is true', () => {
+    const { getByText } = render(<Widget stickyOffset={10} sticky>Test</Widget>)
+    const widget = getByText('Test')
+    expect(widget).toHaveStyleRule('top', '10px')
+  })
+
+  test('stickyOffset gets ignored if sicky is false', () => {
+    const { getByText } = render(<Widget stickyOffset={10}>Test</Widget>)
+    const widget = getByText('Test')
+    expect(widget).not.toHaveStyleRule('top', '10px')
+  })
+
+})
