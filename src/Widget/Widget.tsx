74ca2c4073e92c80752938bdd28dddd51ff2fad9
stefanwullems <stefan@exivity.com>
2019-06-19 09:43:12 +0200

    add sticky to widget

--- src/Widget/Widget.tsx
+++ src/Widget/Widget.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { fromTheme, toRgbString } from '../utils/styled'
 import { Block, BlockProps } from '../Block'
 
-const StyledWidget = styled(Block)`
+interface WidgetProps {
+  sticky?: boolean
+}
+
+const StyledWidget = styled(Block) <WidgetProps>`
   background: white;
   box-shadow: 1px 1px 0 rgba(${fromTheme(theme => toRgbString(theme.global.purposes.primary))},0.4);
   box-sizing: border-box;
+
+  ${props => props.sticky && css`
+    position: sticky;
+  `}
 `
 
 export const Widget = (props: BlockProps & { children: any }) => (
-  <StyledWidget borderRadius={3} p={2} {...props}/>
+  <StyledWidget borderRadius={3} p={2} {...props} />
 )
 
 Widget.displayName = 'Widget'
