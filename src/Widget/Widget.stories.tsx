9c0ed7c92525f00be10aca434175c0b31088aeda
Stefan Wullems <stefan@exivity.com>
2019-06-19 14:25:46 +0200

    Sticky widget (#149)
    * add sticky to widget

--- src/Widget/Widget.stories.tsx
+++ src/Widget/Widget.stories.tsx
@@ -3,10 +3,8 @@ import faker from 'faker'
 import React from 'react'
 
 import { Widget } from '.'
-import { Paragraph } from '../Paragraph'
-import { TextInput } from '../TextInput'
 import { withState } from '../utils/tests/decorators/StateDecorator'
-import { Heading } from '../Heading'
+import { Flex, Heading, Block, TextInput, Paragraph } from '..'
 
 storiesOf('molecules|Widget', module)
   .addDecorator(withState('edit me'))
@@ -15,16 +13,20 @@ storiesOf('molecules|Widget', module)
       <Paragraph>{faker.lorem.paragraphs(4)}</Paragraph>
     </Widget>
   ))
-  .add('no padding', () => (
-    <Widget>
-      <Paragraph>{faker.lorem.paragraphs(4)}</Paragraph>
-    </Widget>
-  ))
   // @ts-ignore
   .add('editable header', ({ state, storeState }: any) => (
     <Widget>
       <Heading as={TextInput} placeholder='placeholder' outlined flat
-        value={state} onChange={(text) => storeState(text)}/>
+        value={state} onChange={(text) => storeState(text)} />
       <Paragraph>{faker.lorem.paragraphs(4)}</Paragraph>
     </Widget>
+  ))
+  .add('sticky', () => (
+    <div style={{ height: 400, overflowY: 'scroll' }}>
+      <Block height={100} />
+      <Widget sticky stickyOffset={10}>
+        <Paragraph>{faker.lorem.paragraphs(4)}</Paragraph>
+      </Widget>
+      <Flex height={1500} justifyContent='flex-end' direction='column'>hi</Flex>
+    </div>
   ))
--- src/Widget/Widget.tsx
+++ src/Widget/Widget.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { fromTheme, toRgbString } from '../utils/styled'
 import { Block, BlockProps } from '../Block'
 
-const StyledWidget = styled(Block)`
+interface WidgetProps {
+  sticky?: boolean
+  stickyOffset?: number
+  children: any
+}
+
+const StyledWidget = styled(Block) <WidgetProps>`
   background: white;
   box-shadow: 1px 1px 0 rgba(${fromTheme(theme => toRgbString(theme.global.purposes.primary))},0.4);
   box-sizing: border-box;
+
+  ${({ sticky, stickyOffset = 0 }) => sticky && css`
+    top: ${stickyOffset}px;
+    position: sticky;
+  `}
 `
 
-export const Widget = (props: BlockProps & { children: any }) => (
-  <StyledWidget borderRadius={3} p={2} {...props}/>
+export const Widget = (props: BlockProps & WidgetProps) => (
+  <StyledWidget borderRadius={3} p={2} {...props} />
 )
 
 Widget.displayName = 'Widget'
