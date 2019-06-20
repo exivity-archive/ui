cc962b2b6c65a45f416353663214804c1d12fa16
ThiagoFazzi <thiago@xtwo.com.br>
2019-06-20 11:22:40 +0200

    all test running ok

--- src/Dropdown/helpers.test.ts
+++ src/Dropdown/helpers.test.ts
@@ -2,9 +2,9 @@ import { makeDefaultCSS } from './helpers'
 import { Vertical, Horizontal } from '../useSnapEdgeToParent'
 
 test('makeCssPosition should return correct css for given inputs', () => {
-  const expected1 = 'top: 10px;\nleft: 0px;\n'
-  const expected2 = 'bottom: 10px;\nright: 0px;\n'
+  const expected1 = 'bottom: 10px;\nright: 0px;\n'
+  const expected2 = 'top: 10px;\nleft: 0px;\n'
 
-  expect(makeDefaultCSS({ horizontal: Horizontal.LEFT, vertical: Vertical.TOP }, 10)).toBe(expected1)
-  expect(makeDefaultCSS({ horizontal: Horizontal.RIGHT, vertical: Vertical.BOTTOM }, 10)).toBe(expected2)
+  expect(makeDefaultCSS({ horizontal: Horizontal.LEFT, vertical: Vertical.TOP }, {} as any, { height: 10 } as any)).toBe(expected1)
+  expect(makeDefaultCSS({ horizontal: Horizontal.RIGHT, vertical: Vertical.BOTTOM }, {} as any, { height: 10 } as any)).toBe(expected2)
 })
