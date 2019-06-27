7a5c24a2c45799c53915f4d0bca8d41f8bc77d83
stefanwullems <stefan@exivity.com>
2019-06-27 15:38:16 +0200

    ggpush

--- src/useClientRect/useClientRect.ts
+++ src/useClientRect/useClientRect.ts
@@ -3,13 +3,17 @@ import { useState, useCallback } from 'react'
 export type Rect = ClientRect | DOMRect
 
 export function useClientRect () {
-  const [rect, setRect] = useState<Rect | null>(null)
+  const [node, setNode] = useState<HTMLElement | null>(null)
 
   const ref = useCallback((node: HTMLElement | null) => {
     if (node !== null) {
-      setRect(node.getBoundingClientRect())
+      setNode(node)
     }
   }, [])
 
-  return [rect, ref] as [Rect | null , typeof ref]
+  const rect = node
+    ? node.getBoundingClientRect()
+    : null
+
+  return [rect, ref] as [Rect | null, typeof ref]
 }
