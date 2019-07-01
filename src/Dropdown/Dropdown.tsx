7863a609d2a9e9dc3967931113a9ed5e8d005ba6
ThiagoFazzi <thiago@xtwo.com.br>
2019-07-01 12:14:48 +0200

    exported content from dropdown component

--- src/Dropdown/Dropdown.tsx
+++ src/Dropdown/Dropdown.tsx
@@ -1,4 +1,9 @@
-import React, { useMemo, cloneElement, isValidElement, ReactElement } from 'react'
+import React, {
+  useMemo,
+  cloneElement,
+  isValidElement,
+  ReactElement
+} from 'react'
 import styled, { css } from 'styled-components'
 
 import { OutsideClickListener } from '../OutsideClickListener'
@@ -7,7 +12,12 @@ import { Block, BlockProps } from '../Block'
 import { makeDefaultCSS } from './helpers'
 
 import { fromTheme } from '../utils/styled'
-import { useSnapEdgeToParent, Vertical, Horizontal, BreakDistance } from '../useSnapEdgeToParent'
+import {
+  useSnapEdgeToParent,
+  Vertical,
+  Horizontal,
+  BreakDistance
+} from '../useSnapEdgeToParent'
 
 const StyledDropdown = styled(Block)`
   position: relative;
@@ -19,7 +29,7 @@ interface ContentProps {
   position?: string | null
 }
 
-const Content = styled(Block) <ContentProps>`
+const Content = styled(Block)<ContentProps>`
   box-sizing: border-box;
   position: absolute;
   background-color: #f9f9f9;
@@ -27,12 +37,17 @@ const Content = styled(Block) <ContentProps>`
   z-index: ${fromTheme(theme => theme.global.zPriority.background)};
 
   min-width: 160px;
-  ${props => props.triggerWidth && css`
-    width: ${props.triggerWidth};
-  `}
+  ${props =>
+    props.triggerWidth &&
+    css`
+      width: ${props.triggerWidth};
+    `}
 
-  visibility: ${({ open }) => open ? 'visible' : 'hidden'};
-  ${({ position }) => css`${position}`}
+  visibility: ${({ open }) => (open ? 'visible' : 'hidden')};
+  ${({ position }) =>
+    css`
+      ${position}
+    `}
 `
 
 export interface DropdownProps extends BlockProps {
@@ -47,7 +62,7 @@ export interface DropdownProps extends BlockProps {
   test?: string
 }
 
-export const Dropdown: React.FC<DropdownProps> = ({
+export const Dropdown: React.FC<DropdownProps> & { Content: typeof Content } = ({
   className,
   triggerComponent,
   children,
@@ -60,29 +75,39 @@ export const Dropdown: React.FC<DropdownProps> = ({
   test = 'dropdown',
   ...blockProps
 }) => {
-  const [{ target, parent }, positioning] = useSnapEdgeToParent(breakDistance, { horizontal, vertical })
+  const [{ target, parent }, positioning] = useSnapEdgeToParent(breakDistance, {
+    horizontal,
+    vertical
+  })
 
-  const position = useMemo(() => parent.rect && makeDefaultCSS(positioning, parent.rect.height), [positioning])
+  const position = useMemo(
+    () => parent.rect && makeDefaultCSS(positioning, parent.rect.height),
+    [positioning]
+  )
 
-  const triggerWidth = parent.rect
-    ? `${parent.rect.width}px`
-    : undefined
+  const triggerWidth = parent.rect ? `${parent.rect.width}px` : undefined
 
   return (
     <StyledDropdown className={className} data-test={test} ref={parent.ref}>
       <OutsideClickListener onOutsideClick={onOutsideClick}>
         {triggerComponent}
-        <Content triggerWidth={useTriggerComponentWidth ? triggerWidth : undefined}
+        <Content
+          triggerWidth={useTriggerComponentWidth ? triggerWidth : undefined}
           data-test={`${test}-content`}
           ref={target.ref}
           position={position}
           open={open}
           {...blockProps}>
           {isValidElement(children)
-            ? cloneElement(children as ReactElement<any>, { ...children.props, positioning })
+            ? cloneElement(children as ReactElement<any>, {
+              ...children.props,
+              positioning
+            })
             : children}
         </Content>
       </OutsideClickListener>
     </StyledDropdown>
   )
 }
+
+Dropdown.Content = Content
