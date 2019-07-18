901ff60679c67bbd42242ac0b0ea2521f5fff627
Michiel <Michiel_87@hotmail.com>
2019-07-18 17:40:03 +0200

    improve outside listener

--- src/useOutsideClickListener/useOutsideClickListener.ts
+++ src/useOutsideClickListener/useOutsideClickListener.ts
@@ -4,22 +4,22 @@ export function useOutsideClickListener (onOutsideClick: Function, node: HTMLDiv
   const ref = useRef<HTMLDivElement>(node)
 
   useEffect(() => {
+    const handleOutsideClick: EventListener = (event: any) => {
+      if (!event.target) {
+        onOutsideClick(event)
+        return ref
+      }
+
+      if (ref.current && !ref.current.contains(event.target)) {
+        onOutsideClick(event)
+      }
+    }
+
     document.addEventListener('click', handleOutsideClick, true)
     return () => {
       document.removeEventListener('click', handleOutsideClick, true)
     }
-  })
-
-  const handleOutsideClick: EventListener = (event: any) => {
-    if (!event.target) {
-      onOutsideClick(event)
-      return ref
-    }
-
-    if (ref.current && !ref.current.contains(event.target)) {
-      onOutsideClick(event)
-    }
-  }
+  }, [onOutsideClick, node])
 
   return ref
 }
