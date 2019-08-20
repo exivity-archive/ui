e71080a080c47bf95d25b6e736c14632f8efbd21
stefanwullems <stefan@exivity.com>
2019-08-20 09:44:18 +0200

    cleanup

--- src/Modal/Modal.stories.tsx
+++ src/Modal/Modal.stories.tsx
@@ -4,13 +4,12 @@ import Faker from 'faker'
 import { storiesOf } from '@storybook/react'
 import { Modal } from './'
 import { Button } from '../Button'
-import { Heading } from '../Heading'
 
 storiesOf('molecules|Modal', module)
   .add('default', () => {
     return (
       <Modal
-        title={<Heading>Disclaimer</Heading>}
+        title='Disclaimer'
         buttons={[
           <Button onClick={() => { return }}>Whatsup</Button>,
           <Button danger onClick={() => { return }}>Delete</Button>
