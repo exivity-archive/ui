534c5afc597f05b310926f24ea7c890830803580
stefanwullems <stefan@exivity.com>
2019-09-24 11:04:47 +0200

    tests

--- src/TreeList/DefaultItem.test.tsx
+++ src/TreeList/DefaultItem.test.tsx
@@ -1,80 +1,82 @@
 import React from 'react'
+import { render, fireEvent, act } from '@testing-library/react'
 
-import { mountWithTheme } from '../utils/tests/mountWithTheme'
-import { TreeList } from './TreeList'
-import { COLLAPSE_ICON, EXPAND_ICON } from './DefaultItem'
+import { DefaultItem } from './DefaultItem'
+import { CHILDREN } from '../utils/makeParentChildTree'
 
-const parentKeyAccessor = (item: any) => item.parentId
-const keyAccessor = (item: any) => item.key
-
-describe('<DefaultItem/>', () => {
+describe('the DefaultItem component', () => {
   test('returns an item onChange', () => {
-    const itemOne = { key: '1', value: 'one', parentId: null }
-
-    const items = [
-      itemOne,
-      { key: '2', value: 'two', parentId: '1' },
-      { key: '3', value: 'three', parentId: '1' },
-      { key: '4', value: 'four', parentId: '1' }
-    ]
+    const item = {
+      key: '1',
+      value: 'one',
+      parentId: null,
+      level: 1,
+      expand: jest.fn(),
+      expanded: false
+    }
 
     const onChange = jest.fn(item => item)
 
-    const wrapper = mountWithTheme(<TreeList keyAccessor={keyAccessor} onChange={onChange} data={items} parentKeyAccessor={parentKeyAccessor} />)
+    const { getByTestId, unmount } = render(<DefaultItem data={{ items: [item], onChange }} style={{}} index={0} />)
 
-    wrapper.find('li')
-      .first()
-      .simulate('click')
+    act(() => {
+      fireEvent.click(getByTestId('treelist-item'))
+    })
 
     const onChangeResult = onChange.mock.results[0].value
 
-    expect(onChangeResult).toMatchObject(itemOne)
+    expect(onChangeResult).toMatchObject(item)
+    unmount()
   })
 
-  test('expands and collapses children when DefaultItem button is clicked', () => {
-    const items = [
-      { key: '1', value: 'one', parentId: null },
-      { key: '2', value: 'two', parentId: '1' },
-      { key: '3', value: 'three', parentId: '1' },
-      { key: '4', value: 'four', parentId: '1' }
-    ]
+  test('shows ToggleExpandedButton when item has children', () => {
+    const item = {
+      key: '1',
+      value: 'one',
+      parentId: null,
+      [CHILDREN]: ['fake'] as any,
+      level: 1,
+      expand: jest.fn(),
+      expanded: false
+    }
 
-    const wrapper = mountWithTheme(<TreeList keyAccessor={keyAccessor} data={items} parentKeyAccessor={parentKeyAccessor} />)
+    const { getByTestId, unmount } = render(<DefaultItem data={{ items: [item], onChange: jest.fn() }} index={0} style={{}} />)
 
-    expect(wrapper.find('li').length).toBe(1)
-
-    wrapper.find('button')
-      .first()
-      .simulate('click')
-
-    expect(wrapper.find('li').length).toBe(4)
-
-    wrapper.find('button')
-      .first()
-      .simulate('click')
-
-    expect(wrapper.find('li').length).toBe(1)
+    expect(getByTestId('treelist-item-toggle-expand-button')).toBeInTheDocument()
+    unmount()
   })
 
-  test('button shows a plus sign when item is not expanded', () => {
-    const item1 = { key: '1', value: 'one', parentId: null }
-    const item2 = { key: '2', value: 'two', parentId: '1' }
+  test('shows expand icon when item is collapsed', () => {
+    const item = {
+      key: '1',
+      value: 'one',
+      parentId: null,
+      [CHILDREN]: ['fake'] as any,
+      level: 1,
+      expand: jest.fn(),
+      expanded: false
+    }
 
-    const wrapper = mountWithTheme(<TreeList keyAccessor={keyAccessor} data={[item1, item2]} parentKeyAccessor={parentKeyAccessor} />)
+    const { getByTestId, unmount } = render(<DefaultItem data={{ items: [item], onChange: jest.fn() }} index={0} style={{}} />)
 
-    expect(wrapper.find(EXPAND_ICON).length).toBe(1)
+    expect(getByTestId('treelist-item-toggle-expand-button-expand-icon')).toBeInTheDocument()
+    unmount()
   })
 
-  test('button shows a minus sign when item is expanded', () => {
-    const item1 = { key: '1', value: 'one', parentId: null }
-    const item2 = { key: '2', value: 'two', parentId: '1' }
+  test('shows collapse icon when item is expanded', () => {
+    const item = {
+      key: '1',
+      value: 'one',
+      parentId: null,
+      [CHILDREN]: ['fake'] as any,
+      level: 1,
+      expand: jest.fn(),
+      expanded: true
+    }
 
-    const wrapper = mountWithTheme(<TreeList keyAccessor={keyAccessor} data={[item1, item2]} parentKeyAccessor={parentKeyAccessor} />)
+    const { getByTestId, unmount } = render(<DefaultItem data={{ items: [item], onChange: jest.fn() }} index={0} style={{}} />)
 
-    wrapper.find('button')
-      .first()
-      .simulate('click')
-
-    expect(wrapper.find(COLLAPSE_ICON).length).toBe(1)
+    expect(getByTestId('treelist-item-toggle-expand-button-collapse-icon')).toBeInTheDocument()
+    unmount()
   })
 })
